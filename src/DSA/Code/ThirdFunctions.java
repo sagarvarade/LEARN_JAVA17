65a941b42e1588cf06409e88f1b8c10d9693d469
sagar_varade <varade.sagar12@gmail.com>
2022-05-26 14:40:51 +0530

    file updated

--- src/DSA/Code/ThirdFunctions.java
+++ src/DSA/Code/ThirdFunctions.java
@@ -21,7 +21,69 @@ public class ThirdFunctions {
 		//productOfTwoNumbers();
 		//getCircumFerenceOfRadius(BigDecimal.valueOf(4.0));
 		//checkPrimeOrNot(13);
-		  getMarksForEachStudent();
+		  //getMarksForEachStudent();
+		//System.out.println(factorial(5));
+		//checkIsPalindromeOrNot(342);
+		//checkIsPalindromeOrNot(343);
+		//returnSumOfFirstNNumber(5);
+		 returnPrimeNumbersFromRange(1000,25000);
+	}
+
+	private static void returnPrimeNumbersFromRange(int start,int end) {
+		for(int i=start+1;i<end;i++) {
+			boolean bl=checkIsPrime(i);
+			if(bl)
+				System.out.println(" Is prime "+i);
+		}
+		
+	}
+
+	private static boolean checkIsPrime(int num) {
+		if(num%2==0)
+			return false;
+		int count=0;
+		for(int i=2;i<Math.sqrt(num);i++) {
+			if(num%i==0)
+				count++;
+		}
+		if(count>0)
+			return false;
+		else
+			return true;
+	}
+
+	private static void returnSumOfFirstNNumber(int n) {
+		int sum=0;
+		for(int i=1;i<=n;i++)
+		{
+			sum=sum+i;
+		}
+		System.out.println(sum);
+	}
+
+	private static void checkIsPalindromeOrNot(final int i) {
+		int temp=i;
+		int rev=0;
+		while(temp!=0) {
+			int reminder=temp%10;
+			rev=reminder+rev*10;
+			temp=temp/10;
+		}
+		System.out.println(rev+"   "+i);
+		if(rev==i)
+			System.out.println("Palindrome");
+		else
+			System.out.println("Not Palindrome");
+		
+		
+	}
+
+	private static int factorial(int n) {
+		if(n<=1)
+			return 1;
+		else
+			return factorial(n-1)*n;
+		
 	}
 
 	private static void getMarksForEachStudent() {
