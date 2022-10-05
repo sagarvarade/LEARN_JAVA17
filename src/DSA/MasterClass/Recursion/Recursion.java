6b447c9bdd56aec860b0928ba063fe293b83a989
sagar_varade <varade.sagar12@gmail.com>
2022-10-05 15:42:54 +0530

    File update

--- src/DSA/MasterClass/Recursion/Recursion.java
+++ src/DSA/MasterClass/Recursion/Recursion.java
@@ -2,8 +2,38 @@ package DSA.MasterClass.Recursion;
 
 public class Recursion {
 	public static void main(String[] args) {
-		openDollRecursion(5);
-		System.out.println("Fact of 5 :"+factorial(5));
+		//openDollRecursion(5);
+		//System.out.println("Fact of 5 :"+factorial(5));
+
+		//System.out.println(PrintSumOfPostiveInteger(1245));
+		//System.out.println(mathPow(2,3));
+		System.out.println(gcd(8,4));
+
+	}
+
+	private static int gcd(int i, int j) {
+		if(j==0)
+		{
+			return i;
+		}
+		return gcd(j,i%j);
+	}
+
+	private static int mathPow(int num, int pow) {
+		if(pow==0 || pow==1)
+		{
+			return num;
+		}
+		else
+			return num*mathPow(num, pow-1);
+	}
+
+	private static int PrintSumOfPostiveInteger(int i) {
+		if(i==0 || i<0)
+			return 0;
+		else
+			return i%10+PrintSumOfPostiveInteger(i/10);
+
 	}
 
 	private static int factorial(int i) {
