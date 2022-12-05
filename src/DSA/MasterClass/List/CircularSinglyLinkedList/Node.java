b861e8f751b3608e7f12d159e7456075a5279335
sagar_varade <varade.sagar12@gmail.com>
2022-12-05 22:57:05 +0530

    List Some files

---
+++ src/DSA/MasterClass/List/CircularSinglyLinkedList/Node.java
@@ -0,0 +1,6 @@
+package DSA.MasterClass.List.CircularSinglyLinkedList;
+
+public class Node {
+	public int value;
+	public Node next;
+}
---
+++ src/DSA/MasterClass/List/SinglyList/Node.java
@@ -0,0 +1,6 @@
+package DSA.MasterClass.List.SinglyList;
+
+public class Node {
+	public int value;
+	public Node next;
+}
