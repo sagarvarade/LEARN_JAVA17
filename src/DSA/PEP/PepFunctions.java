809489afbaecb854ed9ffb86a0f0d6ffe8f22f58
sagar_varade <varade.sagar12@gmail.com>
2023-02-12 20:05:13 +0530

    Files updatd

--- src/DSA/PEP/PepFunctions.java
+++ src/DSA/PEP/PepFunctions.java
@@ -1,10 +1,12 @@
 package DSA.PEP;
 
+import java.util.Scanner;
+
 import org.junit.Test;
 
 public class PepFunctions {
 	
-	@Test
+	//@Test
 	public void decimalToAnyBase() {
 		int num=987;
 		int base=2;
@@ -44,5 +46,68 @@ public class PepFunctions {
 			div=div/10;
 		}
 		System.out.println(count);
+	}
+	
+	public static void main(String str[])
+	{
+		//extracted1();
+		//extraced2();
+		extraceted3();
+	}
+
+
+	private static void extraceted3() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	private static void extraced2() {
+		Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+        	int len=sc.nextInt();
+ 		    char[] ary=(sc.next()).toCharArray();
+ 		    
+ 		    for(int j=0;j<len;j++)
+ 		    {
+ 		        if(ary[j]=='T')
+ 		        ary[j]='A';
+ 		        else if(ary[j]=='A')
+ 		        	ary[j]='T';
+ 		        else if(ary[j]=='G')
+ 		        	ary[j]='C';
+ 		        else if(ary[j]=='C')
+ 		        	ary[j]='G';
+ 		    }
+ 		    
+ 		    System.out.println(String.valueOf(ary));
+		}
+	}
+
+
+	private static void extracted1() {
+		Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+        	int len=sc.nextInt();
+			char [] chary=new char[len];
+			for(int i=0;i<len;i++)
+			{
+			    char c=sc.next().charAt(0);
+			    if(c=='A')
+			        chary[i]='T';
+			    if(c=='T')
+			        chary[i]='A';
+			    if(c=='C')
+			        chary[i]='G';
+			    if(c=='G')
+			        chary[i]='C';
+			}
+			for(int i=0;i<chary.length;i++)
+			{
+			    System.out.print(chary[i]);
+			}
+		}
 	}
 }
--- src/multithreading/completableFutute/CompletableFutureHelloWorldExceptionTest.java
+++ src/multithreading/completableFutute/CompletableFutureHelloWorldExceptionTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 
-
+//@ExtendWith(MockitoExtension.class)
 class CompletableFutureHelloWorldExceptionTest {
 
 	
