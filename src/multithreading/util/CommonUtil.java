396594caed1b30810241a7ed8c98fa972ad90e69
APME-OFFICE\svarade <svarade@euronetworldwide.com>
2023-08-01 11:49:58 +0530

    File updated
    src/multithreading/util/CommonUtil.java
    target/classes/META-INF/maven/LEARN_JAVA17/LEARN_JAVA17/pom.properties

--- src/multithreading/util/CommonUtil.java
+++ src/multithreading/util/CommonUtil.java
@@ -1,42 +1,42 @@
 package multithreading.util;
 
-import static multithreading.util.LoggerUtil.log;
 import static java.lang.Thread.sleep;
+import static multithreading.util.LoggerUtil.log;
 
 import org.apache.commons.lang3.time.StopWatch;
 
 public class CommonUtil {
 
-    public static StopWatch stopWatch = new StopWatch();
+	public static StopWatch stopWatch = new StopWatch();
 
-    public static void delay(long delayMilliSeconds)  {
-        try{
-            sleep(delayMilliSeconds);
-        }catch (Exception e){
-            LoggerUtil.log("Exception is :" + e.getMessage());
-        }
+	public static void delay(long delayMilliSeconds) {
+		try {
+			sleep(delayMilliSeconds);
+		} catch (Exception e) {
+			LoggerUtil.log("Exception is :" + e.getMessage());
+		}
 
-    }
+	}
 
-    public static String transForm(String s) {
-        CommonUtil.delay(500);
-        return s.toUpperCase();
-    }
+	public static String transForm(String s) {
+		CommonUtil.delay(500);
+		return s.toUpperCase();
+	}
 
-    public static void startTimer(){
-        stopWatch.start();
-    }
+	public static void startTimer() {
+		stopWatch.start();
+	}
 
-    public static void timeTaken(){
-        stopWatch.stop();
-        log("Total Time Taken : " +stopWatch.getTime());
-    }
+	public static void timeTaken() {
+		stopWatch.stop();
+		log("Total Time Taken : " + stopWatch.getTime());
+	}
 
-    public static void stopWatchReset(){
-        stopWatch.reset();
-    }
+	public static void stopWatchReset() {
+		stopWatch.reset();
+	}
 
-    public static  int noOfCores(){
-        return Runtime.getRuntime().availableProcessors();
-    }
+	public static int noOfCores() {
+		return Runtime.getRuntime().availableProcessors();
+	}
 }
